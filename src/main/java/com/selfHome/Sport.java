f07e04fd2957c93c13742a8c0ac80d2ed1e38eaf
Dima Bulat <damaskzp@gmail.com>
2019-03-10 23:52:51 +0200

    nrtrt

--- src/main/java/com/selfHome/Sport.java
+++ src/main/java/com/selfHome/Sport.java
@@ -5,7 +5,7 @@ public class Sport {
 
         SportsMan[] sportsMans1 = new SportsMan[3];
 
-        sportsMans1[0] = new Footballer("Pele", "Footbol");
+        sportsMans1[0] = new Footballer("Pele", "Football");
         sportsMans1[1] = new Pilot("Gastello", "Formula");
         sportsMans1[2] = new Chessplayer("Garry", "Chess");
 
